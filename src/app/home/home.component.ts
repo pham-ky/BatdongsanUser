483e82d30dc1e0797f361f887efe3f55ac8397b6
pham-ky <phamky2505@gmail.com>
2021-06-11 10:25:38 +0700

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,30 +6,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, AfterViewInit {
-  public _renderer:any;
-
-  constructor(injector: Injector) {  
-    this._renderer = injector.get(Renderer2);
-    } 
+export class HomeComponent implements OnInit {
+  
+  constructor(){}
   title = "Trang chủ - Bất động sản";
   ngOnInit(): void {
   }
-  public loadScripts() {
-    this.renderExternalScript('../../assets/js/index.bundle.js').onload = () => {
-    }
-  }
-public renderExternalScript(src: string): HTMLScriptElement {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = src;
-    script.async = true;
-    script.defer = true;
-    this._renderer.appendChild(document.body, script);
-    return script;
-  }
-  ngAfterViewInit() { 
-    this.loadScripts();
-  }
+  
 
 }
